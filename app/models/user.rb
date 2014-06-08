1dea6a4c32f6e586e3201161fee5fd8fff963df2
Howard Chen <howardxchen@gmail.com>
2014-06-08 10:02:41 -0700

    Add very basic regex for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
 end
