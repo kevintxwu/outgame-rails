017b2afb2fa82c53699159af64da157198986fb3
dwarrier <dwarrier@gmail.com>
2014-06-08 21:44:00 -0700

    can now update event, still some bugs

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,14 +7,33 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find params[:id]
+  end
+  
+  def edit
+    @event = Event.find params[:id]
   end
 
   def new
   end
 
   def update
+    @event = Event.find params[:id]
+    if @event.update_attributes event_params
+      flash[:success] = 'Event successfully updated!'
+      redirect_to @event
+    else
+      render 'show'
+    end
   end
 
   def destroy
   end
+
+private
+
+def event_params
+  params.require(:event).permit(:name, :date, :bracket, :event_type, :description)
+end
+
 end
