221e6f9635adbb5c9f3d61652462a0e84cf4ee66
Deepak Warrier <dwarrier@Deepaks-MacBook-Pro-2.local>
2014-06-21 14:47:30 -0700

    some more changes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game
   include ActiveModel::Model
-  attr_accessor :player_1, :player_2, :round_number
+  attr_accessor :player_1, :player_2, :result
+
+  def initialize(attributes = {})
+    @result = 'T' #represents a tie.
+  end
 end
