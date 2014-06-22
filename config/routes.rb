4bcfadd4a5e1bda91fbc8cb3ce2ef1446c5df255
Howard Chen <howardxchen@gmail.com>
2014-06-21 17:41:29 -0700

    Fix routing for signout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   match '/signup', to: 'users#new',  via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
-  match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/signout', to: 'sessions#destroy', via: 'get', via: 'delete'
 
   get 'template/index', :path => 'template'
   get 'template/new'
