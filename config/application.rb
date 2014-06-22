5990334de882e965547b91a9f48d66151f26a598
Kevin Wu <kevintxwu@gmail.com>
2014-06-21 17:24:44 -0700

    attempting to fix assets on heroku

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# config/application.rb
-config.assets.initialize_on_precompile = false
-
 module OutgameRails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +19,7 @@ module OutgameRails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
   end
 end
