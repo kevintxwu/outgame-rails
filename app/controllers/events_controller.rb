34cf00a5a7388ea862001aedb5848647947efa9e
Deepak Warrier <dwarrier@Deepaks-MacBook-Pro-2.local>
2014-07-31 23:51:07 -0700

    couple of bug fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,7 +65,8 @@ class EventsController < ApplicationController
   def save_event
     @event = Event.find params[:id]
     if @event.update_attributes event_params
-      render 'show-bracket'
+      @event.calculate_scores
+      redirect_to :action => :show_bracket
     else
       render 'show' #for error checks
     end
